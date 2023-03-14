cf94504cdb9b0ded66c7fc44f931d97ff0041d51
Krzysztof R <plkrozbicki@gmail.com>
2023-03-14 21:31:16 +0100

    Create task 1 - simplelightbox gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = '';
+for (const image of galleryItems) {
+  const newImage = `<a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>`;
+  galleryEl.innerHTML += newImage;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 200 });
