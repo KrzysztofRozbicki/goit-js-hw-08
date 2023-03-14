30019baaca3645396b575840d256f6bf83cce95e
Krzysztof R <plkrozbicki@gmail.com>
2023-03-14 23:24:38 +0100

    Video task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoEl = document.getElementById('vimeo-player');
+const video = new Player(videoEl);
+const updateTimeDelay = throttle(
+  data => localStorage.setItem('videoplayer-current-time', data.seconds),
+  1000
+);
+
+video.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then();
+video.on('timeupdate', updateTimeDelay);
